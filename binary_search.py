96f9939545b6151a7fa5096cdc79eccfee755969
Alevtin <antonov3017@gmail.com>
2023-03-14 16:33:14 +0300

    2nd comment

--- binary_search.py
+++ binary_search.py
@@ -19,3 +19,21 @@ print(binary_search(my_list, 3))
 print(binary_search(my_list, 9))
 print(binary_search(my_list, 6))
 print(binary_search(my_list, 1))
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5, 3, 2, 10]))
